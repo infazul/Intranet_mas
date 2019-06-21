862eab05a77984a74a887074f656a523be9ab562
Fabius-code <52058851+Fabius-code@users.noreply.github.com>
2019-06-21 01:41:20 -0400

    Add files via upload

--- intranet_mas/modificar_usuarios.php
+++ intranet_mas/modificar_usuarios.php
@@ -1,28 +1,90 @@
 <?php 
     require 'database.php';
+
+    $mensaje = '';
+
+    if (!empty($_POST['rut']) && !empty($_POST['password']) && !empty($_POST['primer_nombre']) && !empty($_POST['primer_apellido']) ){
+        
+        $sql = "UPDATE usuarios SET rut=':rut',password=':password',perfil=':perfil',primer_nombre=':primer_nombre',segundo_nombre=':segundo_nombre',primer_apellido=':primer_apellido',segundo_apellido=':segundo_apellido' WHERE id=':id_u'";
+        $stmt = $conn->prepare($sql);
+        
+        $id_u = $_POST['id_u'];
+        $rut = $_POST['rut'];
+        $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+        $perfil = $_POST['perfil'];
+        $primer_nombre = $_POST['primer_nombre'];
+        $segundo_nombre = $_POST['segundo_nombre'];
+        $primer_apellido = $_POST['primer_apellido'];
+        $segundo_apellido = $_POST['segundo_apellido'];
+
+        $stmt->bindParam(':id_u', $id);
+        $stmt->bindParam(':rut', $rut);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':perfil', $perfil);
+        $stmt->bindParam(':primer_nombre', $primer_nombre);
+        $stmt->bindParam(':segundo_nombre', $segundo_nombre);
+        $stmt->bindParam(':primer_apellido', $primer_apellido);
+        $stmt->bindParam(':segundo_apellido', $segundo_apellido);
+        
+        if ($stmt->execute()) {
+            $mensaje = 'Usuario modificado satisfactoriamente';
+            
+        } else {
+            $mensaje = 'No se ha podido modificar usuario' . $id_u;
+        }
+
+    }
+
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Modificar Usuarios</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+
 </head>
 <body>
     <form action="modificar_usuarios.php" method="post">
-        <?php 
+        <?php             
+            require 'header.php';     
+        ?>
+        <?php if(!empty($mensaje)): ?>
+            <p> <?= $mensaje ?></p>
+        <?php endif; ?>
+        <?php            
             echo '<div class="caja">';
-            echo '<select name="id">';
+            echo '<select name="id_u">';
             echo '<option value="">Seleccionar </option>';
             $sql = "SELECT * FROM usuarios ";
             $stmt = $conn->prepare($sql);
             $stmt->execute();
              foreach ($stmt as $row) {
-                echo '<option value="'.$row['id'].'">'.$row['rut'].' - '.$row['primer_nombre'].' '.$row['primer_apellido'].'</option>';
+                echo '<option value="'.$row['id'].'">'.$row['rut'].'   '.$row['primer_nombre'].' '.$row['primer_apellido'].'</option>';
              }
             echo '</select>';
             echo '</div>';
         ?>
+        Ingrese datos a modificar
+        <input type="text" id="rut" name="rut" placeholder="Ingrese el rut">
+        <input type="password" name="password" placeholder="Ingrese una contraseña">
+        <div class="caja">
+            <select name="perfil" id="p">
+                <option value="3">Alumno</option>
+                <option value="2">Apoderado</option>
+                <option value="1">Profesor</option>
+                <option value="0">Administrador</option>
+            </select>
+        </div>
+        <input type="text" name="primer_nombre" placeholder="Primer Nombre">
+        <input type="text" name="segundo_nombre" placeholder="Segundo Nombre">
+        <input type="text" name="primer_apellido" placeholder="Primer Apellido">
+        <input type="text" name="segundo_apellido" placeholder="Segundo Apellido">
         <input type="submit" value="Guardar">
     </form>
 
